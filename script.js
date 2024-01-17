4d2c5c7f3c61953c0f84d990232228b3bf6f4e07
Linkdom <donglinkang2021@163.com>
2024-01-17 12:38:20 +0800

    get basic effect

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     const images = document.querySelectorAll('.image');
-
+    const image_container = document.querySelector('.image-container');
     images.forEach(image => {
         image.addEventListener('mouseover', function() {
             // 获取对应的image_x并设置为背景图片
             const imageId = image.id.replace('image_', '');
-            image.style.backgroundImage = `url('learn/gen/image_${imageId}.jpg')`;
+            image_container.style.backgroundImage = `url('learn/gen/image_${imageId}.jpg')`;
         });
 
         image.addEventListener('mouseout', function() {
             // 恢复透明图片
-            image.style.backgroundImage = "url('learn/gen/transparent_image.png')";
+            image_container.style.backgroundImage = "url('learn/gen/transparent_image.png')";
         });
     });
 });
